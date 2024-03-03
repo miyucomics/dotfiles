289d2c3998c06b9082f950d4b96b609d2c01c09d
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-02 21:45:01 -0500

    Fixed even more AGS stuff

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,50 +1,12 @@
 const audio = await Service.import("audio")
-const hyprland = await Service.import("hyprland")
 const mpris = await Service.import("mpris")
+
+import clientTitle from "./modules/clientTitle.ts"
+import workspaces from "./modules/workspaces.ts"
 
 const date = Variable("", {
     poll: [1000, 'date "+%H:%M:%S %b %e."']
 })
-
-const winOnWork = id => {
-    hyprland.clients.forEach(client => {
-        if (client.workspace.id == id) {
-            console.log("Has stuff", id)
-            return true
-        }
-    })
-    return false
-}
-
-const Workspaces = () => {
-    const activeId = hyprland.active.workspace.bind("id")
-    return Widget.EventBox({
-        onScrollUp: () => {
-            if (hyprland.active.workspace.id < 9) hyprland.messageAsync(`dispatch workspace +1`)
-        },
-        onScrollDown: () => {
-            if (hyprland.active.workspace.id > 0) hyprland.messageAsync(`dispatch workspace -1`)
-        },
-        child: Widget.Box({
-            class_name: "workspaces",
-            spacing: 15,
-            children: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(id =>
-                Widget.EventBox({
-                    on_primary_click: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-                    child: Widget.Icon({ icon: activeId.as(i => `radio-${i == id ? "checked-" : ""}symbolic`) })
-                        .hook(hyprland, (self, _) => (self.class_name = `${winOnWork(id) ? "full" : ""}`), "client-added")
-                        .hook(hyprland, (self, _) => (self.class_name = `${winOnWork(id) ? "full" : ""}`), "client-removed")
-                })
-            )
-        })
-    })
-}
-
-const ClientTitle = () =>
-    Widget.Label({
-        class_name: "client-title",
-        label: hyprland.active.client.bind("title")
-    })
 
 const Clock = () =>
     Widget.Label({
@@ -99,38 +61,31 @@ const Volume = () =>
         ]
     })
 
-const Left = () =>
-    Widget.Box({
-        spacing: 8,
-        children: [Workspaces(), Media()]
-    })
-
-const Center = () =>
-    Widget.Box({
-        spacing: 8,
-        children: [ClientTitle()]
-    })
-
-const Right = () =>
-    Widget.Box({
-        hpack: "end",
-        spacing: 8,
-        children: [Volume(), Clock()]
-    })
-
 const Bar = () =>
     Widget.Window({
         name: "bar",
         class_name: "bar",
         anchor: ["top", "left", "right"],
         child: Widget.CenterBox({
-            start_widget: Left(),
-            center_widget: Center(),
-            end_widget: Right()
+            start_widget:
+                Widget.Box({
+                    spacing: 8,
+                    children: [workspaces(), Media()]
+                }),
+            center_widget:
+                Widget.Box({
+                    spacing: 8,
+                    children: [clientTitle()]
+                }),
+            end_widget: Widget.Box({
+                hpack: "end",
+                spacing: 8,
+                children: [Volume(), Clock()]
+            })
         })
     })
 
-export default {
-    style: "./style.css",
-    windows: [Bar()]
-}
+    export default {
+        style: "./style.css",
+        windows: [Bar()]
+    }
---
+++ .config/ags/modules/clientTitle.ts
@@ -0,0 +1,7 @@
+const hyprland = await Service.import("hyprland")
+
+export default () =>
+    Widget.Label({
+        class_name: "client-title",
+        label: hyprland.active.client.bind("title")
+    })
