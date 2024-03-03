bcfc302466ad2ba83226ac9d758068591f8e2f39
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-02 22:00:08 -0500

    Final round of AGS for the day

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,91 +1,32 @@
-const audio = await Service.import("audio")
-const mpris = await Service.import("mpris")
-
-import clientTitle from "./modules/clientTitle.ts"
 import workspaces from "./modules/workspaces.ts"
-
-const date = Variable("", {
-    poll: [1000, 'date "+%H:%M:%S %b %e."']
-})
-
-const Clock = () =>
-    Widget.Label({
-        class_name: "clock",
-        label: date.bind()
-    })
-
-const Media = () =>
-    Widget.EventBox({
-        class_name: "media",
-        on_primary_click: () => mpris.getPlayer("")?.playPause(),
-        on_scroll_up: () => mpris.getPlayer("")?.next(),
-        on_scroll_down: () => mpris.getPlayer("")?.previous(),
-        child: Widget.Label("-").hook(
-            mpris,
-            self => {
-                if (mpris.players[0]) {
-                    const { track_artists, track_title } = mpris.players[0]
-                    self.label = `${track_artists.join(", ")} - ${track_title}`
-                } else {
-                    self.label = "Nothing is playing"
-                }
-            },
-            "player-changed"
-        )
-    })
-
-const Volume = () =>
-    Widget.Box({
-        class_name: "volume",
-        children: [
-            Widget.Icon().hook(audio.speaker, self => {
-                const mapping = {
-                    101: "overamplified",
-                    67: "high",
-                    34: "medium",
-                    1: "low",
-                    0: "muted"
-                }
-
-                const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(threshold => threshold <= audio.speaker.volume * 100)
-
-                self.icon = `audio-volume-${mapping[icon]}-symbolic`
-            }),
-            Widget.Label({
-                hexpand: true,
-                setup: self =>
-                    self.hook(audio.speaker, () => {
-                        self.label = Math.floor((audio.speaker.volume || 0) * 100).toString()
-                    })
-            })
-        ]
-    })
+import media from "./modules/media.ts"
+import title from "./modules/title.ts"
+import volume from "./modules/volume.ts"
+import clock from "./modules/clock.ts"
 
 const Bar = () =>
-    Widget.Window({
-        name: "bar",
-        class_name: "bar",
-        anchor: ["top", "left", "right"],
-        child: Widget.CenterBox({
-            start_widget:
-                Widget.Box({
-                    spacing: 8,
-                    children: [workspaces(), Media()]
-                }),
-            center_widget:
-                Widget.Box({
-                    spacing: 8,
-                    children: [clientTitle()]
-                }),
-            end_widget: Widget.Box({
-                hpack: "end",
-                spacing: 8,
-                children: [Volume(), Clock()]
-            })
-        })
-    })
+	Widget.Window({
+		name: "bar",
+		class_name: "bar",
+		anchor: ["top", "left", "right"],
+		child: Widget.CenterBox({
+			start_widget: Widget.Box({
+				spacing: 8,
+				children: [workspaces(), media()]
+			}),
+			center_widget: Widget.Box({
+				spacing: 8,
+				children: [title()]
+			}),
+			end_widget: Widget.Box({
+				hpack: "end",
+				spacing: 8,
+				children: [volume(), clock()]
+			})
+		})
+	})
 
-    export default {
-        style: "./style.css",
-        windows: [Bar()]
-    }
+export default {
+	style: "./style.css",
+	windows: [Bar()]
+}
---
+++ .config/ags/modules/volume.ts
@@ -0,0 +1,26 @@
+const audio = await Service.import("audio")
+
+export default () =>
+    Widget.Box({
+        class_name: "volume",
+        children: [
+            Widget.Icon().hook(audio.speaker, self => {
+                const mapping = {
+                    101: "overamplified",
+                    67: "high",
+                    34: "medium",
+                    1: "low",
+                    0: "muted"
+                }
+                const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(threshold => threshold <= audio.speaker.volume * 100)
+                self.icon = `audio-volume-${mapping[icon]}-symbolic`
+            }),
+            Widget.Label({
+                hexpand: true,
+                setup: self =>
+                    self.hook(audio.speaker, () => {
+                        self.label = Math.floor((audio.speaker.volume || 0) * 100).toString()
+                    })
+            })
+        ]
+    })
