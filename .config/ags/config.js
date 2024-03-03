c8ebb8ace48d0b5a81bf68eb56c4216384b6f0a1
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-02 21:12:15 -0500

    More AGS

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,23 +1,42 @@
+const audio = await Service.import("audio")
 const hyprland = await Service.import("hyprland")
 const mpris = await Service.import("mpris")
-const audio = await Service.import("audio")
 
 const date = Variable("", {
     poll: [1000, 'date "+%H:%M:%S %b %e."']
 })
 
-function Workspaces() {
+const winOnWork = id => {
+    hyprland.clients.forEach(client => {
+        if (client.workspace.id == id) {
+            console.log("Has stuff", id)
+            return true
+        }
+    })
+    return false
+}
+
+const Workspaces = () => {
     const activeId = hyprland.active.workspace.bind("id")
-    return Widget.Box({
-        class_name: "workspaces",
-        children: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(id =>
-            Widget.Button({
-                on_clicked: () =>
-                    hyprland.messageAsync(`dispatch workspace ${id}`),
-                child: Widget.Icon("radio-symbolic"),
-                class_name: activeId.as(i => `${i === id ? "focused" : ""}`)
-            })
-        )
+    return Widget.EventBox({
+        onScrollUp: () => {
+            if (hyprland.active.workspace.id < 9) hyprland.messageAsync(`dispatch workspace +1`)
+        },
+        onScrollDown: () => {
+            if (hyprland.active.workspace.id > 0) hyprland.messageAsync(`dispatch workspace -1`)
+        },
+        child: Widget.Box({
+            class_name: "workspaces",
+            spacing: 15,
+            children: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(id =>
+                Widget.EventBox({
+                    on_primary_click: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+                    child: Widget.Icon({ icon: activeId.as(i => `radio-${i == id ? "checked-" : ""}symbolic`) })
+                        .hook(hyprland, (self, _) => (self.class_name = `${winOnWork(id) ? "full" : ""}`), "client-added")
+                        .hook(hyprland, (self, _) => (self.class_name = `${winOnWork(id) ? "full" : ""}`), "client-removed")
+                })
+            )
+        })
     })
 }
 
@@ -34,7 +53,7 @@ const Clock = () =>
     })
 
 const Media = () =>
-    Widget.Button({
+    Widget.EventBox({
         class_name: "media",
         on_primary_click: () => mpris.getPlayer("")?.playPause(),
         on_scroll_up: () => mpris.getPlayer("")?.next(),
@@ -66,11 +85,7 @@ const Volume = () =>
                     0: "muted"
                 }
 
-                const icon = audio.speaker.is_muted
-                    ? 0
-                    : [101, 67, 34, 1, 0].find(
-                        threshold => threshold <= audio.speaker.volume * 100
-                    )
+                const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(threshold => threshold <= audio.speaker.volume * 100)
 
                 self.icon = `audio-volume-${mapping[icon]}-symbolic`
             }),
@@ -78,9 +93,7 @@ const Volume = () =>
                 hexpand: true,
                 setup: self =>
                     self.hook(audio.speaker, () => {
-                        self.label = Math.floor(
-                            (audio.speaker.volume || 0) * 100
-                        ).toString()
+                        self.label = Math.floor((audio.speaker.volume || 0) * 100).toString()
                     })
             })
         ]
