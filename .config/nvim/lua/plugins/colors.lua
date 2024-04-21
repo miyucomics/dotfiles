939a5f30dc9592407a8b95ec63c7a6eff2e25c4b
Miyu Nguyen <nbriannyc@gmail.com>
2024-04-21 18:37:20 -0400

    Computer tweaks

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,9 +1,12 @@
 return {
-    "catppuccin/nvim",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({})
-        vim.o.termguicolors = true
-        vim.cmd.colorscheme("catppuccin")
-    end,
+	"catppuccin/nvim",
+	priority = 1000,
+	opts = {
+		transparent_background = true,
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.o.termguicolors = true
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
