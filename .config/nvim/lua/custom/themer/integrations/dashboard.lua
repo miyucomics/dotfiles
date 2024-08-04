309b2785620dac155e6e52211dbff4f1cde88eb8
miyucomics <nbriannyc@gmail.com>
2024-08-03 21:43:20 -0400

    Dashboard looks better

--- .config/nvim/lua/custom/themer/integrations/dashboard.lua
+++ .config/nvim/lua/custom/themer/integrations/dashboard.lua
@@ -1,6 +1,7 @@
 local colors = require("custom.themer").get_theme_tb("base_30")
 
 return {
-    DashHeader = { fg = colors.green },
-    DashButtons = { fg = colors.white },
+    DashHeader = { fg = colors.blue },
+    DashButtons = { fg = colors.greyfg2, italic = true },
+    DashQuote = { fg = colors.blue, italic = true },
 }
---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,25 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<c-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		for i = 1, 9 do
+			vim.keymap.set("n", "<a-" .. tostring(i) .. ">", function()
+				harpoon:list():select(i)
+			end)
+		end
+	end,
+}
