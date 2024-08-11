4929eff4b1cbae04e8c041b23383ed52050ccf14
miyucomics <nbriannyc@gmail.com>
2024-08-10 21:58:34 -0400

    Final dotfiles for the night

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,9 @@ require("core.keymap")
 require("core.bootstrap")
 
 require("lazy").setup("plugins", {
+	ui = {
+		backdrop = 100,
+	},
 	performance = {
 		rtp = {
 			disabled_plugins = {
@@ -22,29 +25,4 @@ require("lazy").setup("plugins", {
 	},
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		if vim.fn.argc() == 0 then
-			require("custom.dashboard")()
-		end
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-	callback = function()
-		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-		if not normal.bg then
-			return
-		end
-		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-	end,
-})
-
-vim.api.nvim_create_autocmd("UILeave", {
-	callback = function()
-		io.write("\027]111\027\\")
-	end,
-})
-
 require("custom.themer").set_theme("catppuccin-mocha")
-
--- .config/nvim/lua/custom/themer.lua
+++ .config/nvim/lua/custom/themer.lua
@@ -1,6 +1,31 @@
 local Themer = {}
 local current_theme = {}
 
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.argc() == 0 then
+			require("custom.dashboard")()
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if not normal.bg then
+			return
+		end
+		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+	end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+	callback = function()
+		io.write("\027]111\027\\")
+	end,
+})
+
+
 Themer.set_theme = function(name)
 	local present, theme = pcall(require, "custom.themer.themes." .. name)
 	if not present then
