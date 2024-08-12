215966388b60c3927c8f7123e13f1832f85ffb30
miyucomics <nbriannyc@gmail.com>
2024-08-11 21:17:09 -0400

    Save counter

--- .config/nvim/lua/custom/congratulator.lua
+++ .config/nvim/lua/custom/congratulator.lua
@@ -1,32 +1,21 @@
-local congratulations = {
-	" Great job! ✨ ",
-	" You're doing great! 💪 ",
-	" Keep up the good work! 🌟 ",
-	" Well done! 🎉 ",
-	" Onward and upward! 🚀 ",
-	" You're on fire! 🔥 ",
-	" You're a star! ⭐️ ",
-	" You're amazing! 🌈 ",
-	" That was awesome! 🎈 ",
-	" Smart move. 🧠 ",
-	" Bravo! 👏 ",
-	" Nailed it. 🔨 ",
-}
+local saveCounter = 0
 
 vim.api.nvim_create_autocmd("BufWritePost", {
 	group = vim.api.nvim_create_augroup("Congratulations", { clear = true }),
 	callback = function()
-		local message = congratulations[math.random(#congratulations)]
+		saveCounter = saveCounter + 1
+
+		local message = "Save #" .. tostring(saveCounter) .. "!"
 		local width = #message
 		local buf = vim.api.nvim_create_buf(false, true)
 		local win = vim.api.nvim_open_win(buf, false, {
 			style = "minimal",
 			border = "rounded",
-			relative = "editor",
+			relative = "cursor",
 			height = 1,
 			width = width,
-			col = 0,
 			row = 0,
+			col = 0,
 		})
 		vim.api.nvim_buf_set_lines(buf, 0, -1, false, { message })
 
