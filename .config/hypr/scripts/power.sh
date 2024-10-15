ca1e6ace0af1b57c9412cf6126a5c2b7d4cc9ece
miyucomics <nbriannyc@gmail.com>
2024-10-15 19:49:05 -0400

    Lock

--- .config/hypr/scripts/power.sh
+++ .config/hypr/scripts/power.sh
@@ -1,6 +1,9 @@
-chosen=$(echo -e "Suspend\nExit\nShutdown\nRestart" | tofi --prompt-text "Power:")
+chosen=$(echo -e "Lock\nSuspend\nExit\nShutdown\nRestart" | tofi --prompt-text "Power:")
 
 case "$chosen" in
+    "Lock")
+        hyprlock
+        ;;
     "Suspend")
         systemctl suspend
         ;;
