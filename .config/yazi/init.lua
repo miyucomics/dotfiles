d7402c4ca8044c517d21807bb09fd60c2837d793
miyucomics <nbriannyc@gmail.com>
2025-05-16 16:09:52 -0400

    Grey out unloaded tabs

--- .config/yazi/init.lua
+++
@@ -1,9 +0,0 @@
-local old_layout = Tab.layout
-
-Status.render = function()
-	return {}
-end
-Tab.layout = function(self, ...)
-	self._area = ui.Rect({ x = self._area.x, y = self._area.y, w = self._area.w, h = self._area.h + 1 })
-	return old_layout(self, ...)
-end
